a7b0d0aba68576c3f6f16cd45d750a8bb1a714a3
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-23 10:18:57 -0300

    correção v1

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,7 +1,8 @@
-async function listaVideos(){
+async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
-    const conexaoConverstida = await conexao.json();
-    return conexaoConverstida;
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 }
 
 export const conectaAPI = {
